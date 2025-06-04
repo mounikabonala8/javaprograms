9ed057ac09d66d3f110daed27e7f17ecef033164
mounikabonala8 <bonalamounika53@gmail.com>
2025-06-04 14:52:24 +0530

    Add files via upload

---
+++ Comparetwostring.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    String a=sc.nextLine();
+	    String b=sc.nextLine();
+	    
+	        if(a.length()!=b.length()){
+	            System.out.println(false);
+	            System.exit(0);
+	        }
+	        for(int i=0;i<a.length();i++){
+	            char ch=a.charAt(i);
+	            if(ch!=b.charAt(i)){
+	             System.out.println(false);  
+	            System.exit(0); 
+	            }
+	        }
+	         System.out.println(true);
+	    
+	}
