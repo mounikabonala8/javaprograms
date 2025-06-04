818b0036a3a5156f9752f2c0f8eca8d106c2646b
mounikabonala8 <bonalamounika53@gmail.com>
2025-06-04 14:58:58 +0530

    Add files via upload

---
+++ Queueusingcollections.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Queueusingcollections
+{
+	public static void main(String[] args) {
+	    Queue <Integer> q=new LinkedList();
+	    q.add(1);
+	    q.add(2);
+	     System.out.println(q);
+	    
+	    System.out.println(q.remove());
+	  
+	}   
+	}
